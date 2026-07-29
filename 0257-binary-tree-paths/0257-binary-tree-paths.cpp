82c28e4b726fe4c27540fbb506d47a7a898cedd1
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-07-29 12:21:53 +0530

    Time: 0 ms (100%), Space: 17.9 MB (24.09%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,35 +11,27 @@
  */
 class Solution {
 public:
-   void helper(TreeNode * root,string  & ans ,vector<string>&s){
- if(root->left==NULL && root->right==NULL){
-            int len = ans.length();
-            ans = ans+to_string(root->val); 
-            s.push_back(ans);
-            
-        ans.resize(len);
-            return ;
-        }
-        
-        int len = ans.length();
-        ans = ans+to_string(root->val)+"->";
-        if(root->left!=NULL){
-        helper(root->left,ans,s);
-        }
-        if(root->right!=NULL){ 
-        helper(root->right,ans,s);
-        }
-        ans.resize(len);
-        
-        
+void helper(TreeNode * root ,string ans , vector<string>& st){
+if(root==NULL){
+    return ;
+}
+string str  = to_string(root->val);
+if(root->left==NULL && root->right==NULL){
+    ans+=str ;
+    st.push_back(ans);
+    return ;
+}
+    helper(root->left,ans+str+"->",st);
+    helper(root->right,ans+str+"->",st);
+
+
+
 }
     vector<string> binaryTreePaths(TreeNode* root) {
-        string ans ="";
-        vector<string>s ;
-        helper(root,ans,s);
-        return s ;
+        string ans="";
+        vector<string>st ;
+        helper(root,ans,st);
+        return st ;
 
-
-        
     }
 };
