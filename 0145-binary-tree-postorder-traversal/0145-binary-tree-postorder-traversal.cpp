1b45e6ff8bc9a5d52a2d318c55296410e1389722
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-07-31 17:29:01 +0530

    Time: 0 ms (100%), Space: 10.9 MB (93.73%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,17 +11,32 @@
  */
 class Solution {
 public:
-void postorder(TreeNode * root, vector<int> & ans){
-    if(root==NULL){
-        return ;
-    }
-    postorder(root->left,ans);
-    postorder(root->right,ans);
-    ans.push_back(root->val);
-}
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>ans ;
-        postorder(root,ans);
-        return ans ;
+        if(root==NULL){
+            return ans ;
+        }
+        stack<TreeNode * >st ;
+
+        st.push(root);
+        while(st.size()>0){
+            TreeNode * temp = st.top() ;
+            st.pop();
+            ans.push_back(temp->val);
+
+            if(temp->left){
+                st.push(temp->left);
+            }
+            if(temp->right){
+                st.push(temp->right);
+
+            }
+            
+
+        }
+    reverse(ans.begin(),ans.end());
+    return ans ;
+
+        
     }
 };
