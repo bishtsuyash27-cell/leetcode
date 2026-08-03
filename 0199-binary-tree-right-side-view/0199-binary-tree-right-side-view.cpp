a6aa316eef9cb12cd92d6e825ff5f6cd57f5a5e5
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-03 20:06:26 +0530

    Time: 0 ms (100%), Space: 14.7 MB (99.78%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -15,32 +15,22 @@ int level(TreeNode * root){
     if(root==NULL){
         return 0 ;
     }
-    return 1+ max(level(root->left),level(root->right));
+    return 1 +max(level(root->left),level(root->right));
 }
-void nthlevel(TreeNode * root,  int curr , int level,vector<int>&ans){
+void preorder(TreeNode * root, vector<int>&ans, int level){
     if(root==NULL){
         return ;
     }
-    if(curr==level){
-        ans[curr] = root->val;
-    }
-    nthlevel(root->left,curr+1,level,ans);
-    nthlevel(root->right,curr+1,level,ans);
-}
-void levelorder(TreeNode * root , vector<int>&ans){
-    int n = ans.size();
-    for(int i = 0 ; i<n; i++){
-        nthlevel(root,0,i,ans);
-    }
+    ans[level] = root->val ;
+    preorder(root->left,ans,level+1);
+    preorder(root->right,ans,level+1);
 }
     vector<int> rightSideView(TreeNode* root) {
         
-        int n = level(root);
+        vector<int>ans(level(root));
 
-        vector<int>ans(n);
-        levelorder(root,ans);
-return ans ;
-
-
+        preorder(root,ans,0);
+        return ans ;
+        
     }
 };
