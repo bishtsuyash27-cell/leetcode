3c96ea7cb141512863fdfb5f8bfe5306d12d3656
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-05 12:41:19 +0530

    Time: 13 ms (27.6%), Space: 19 MB (77.2%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,27 +11,29 @@
  */
 class Solution {
 public:
-void helper(TreeNode * root,long long   sum , int& count ){
+void helper(TreeNode * root,long long  targetSum , int &count){
     if(root==NULL){
-        return  ;
+        return ;
     }
-    if(root->val==sum){
+    if(root->val==targetSum){
         count++;
-        
+   
     }
-    helper(root->left,sum -  root->val,  count);
-    helper(root->right,sum - root->val, count);
+    
+    helper(root->left,targetSum-root->val,count);
+    helper(root->right,targetSum-root->val,count);
 
 }
-
     int pathSum(TreeNode* root, int targetSum) {
         if(root==NULL){
-            return  0 ;
+            return 0  ;
+
         }
         int count = 0 ;
         helper(root,targetSum,count);
         count+=pathSum(root->left,targetSum);
         count+=pathSum(root->right,targetSum);
         return count ;
+
     }
 };
