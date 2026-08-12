ef7941154d293c3a4cbfdc5357b4db784959539a
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-12 19:07:33 +0530

    Time: 0 ms (100%), Space: 27.3 MB (53.44%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
-        int i = 0 ;
-        int j = s.size()-1 ;
-        while(i<j){
-            char temp = s[i];
-            s[i] = s[j] ;
-            s[j] = temp ;
-            i++;
-            j--;
-        }
-    }
+        int n = s.size() ;
 
+
+        for(int i = 0 ; i<n/2 ; i++){
+            char temp = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = temp;
+        }
+        
+    }
 };
