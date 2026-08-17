b30350c8cc2fe3c7f9552f35b48331285a2ffe20
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-17 17:33:59 +0530

    Time: 0 ms (100%), Space: 24.8 MB (87.41%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -1,32 +1,57 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-
-    void trim(TreeNode* root, int lo, int hi){
-        if(root==NULL) return;
-        while(root->left!=NULL){
-            if(root->left->val < lo)
-                root->left = root->left->right;
-            else if(root->left->val > hi)
-                root->left = root->left->left;
-            else break;
-        }
-        while(root->right!=NULL){
-            if(root->right->val > hi)
-                root->right = root->right->left;
-            else if(root->right->val < lo)
-                root->right = root->right->right;
-            else break;
-        }
-        trim(root->left,lo,hi);
-        trim(root->right,lo,hi);
+void trim(TreeNode * root,int low , int high){
+    if(root==NULL){
+        return ; 
     }
+
+    while(root->left){
+        if(root->left->val < low){
+            root->left = root->left->right ;
+        }
+        
+
+       else  if(root->left->val > high){
+            root->left = root->left->left ;
+        }
+        else{
+            break ;
+        }
+    }
+
+    while(root->right){
+        if(root->right->val>high){
+            root->right = root->right->left ;
+        }
+        else if (root->right->val < low){
+            root->right = root->right->right ;
+        }
+        else{
+            break ;
+        }
+
+
+    }
+        trim(root->left,low,high);
+        trim(root->right,low,high);
+
+}
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        TreeNode* dummy = new TreeNode(INT_MAX);
-        dummy->left = root;
+        TreeNode * dummy = new TreeNode(INT_MAX);
 
-        trim(dummy, low, high);
-
-       return dummy->left ;
+        dummy->left = root ;
+        trim(dummy,low,high);
+        return dummy->left ;
     }
 };
