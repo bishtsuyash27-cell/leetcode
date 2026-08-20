7a105587f8758d37efabd6dbb8fb35d4953d2b13
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-20 17:59:48 +0530

    Time: 116 ms (17.63%), Space: 19.1 MB (31.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxcount = 0 ; 
-    vector<int>freq(256,0);
-    int count = 0 ;
-    for(int i = 0 ; i<s.size();i++){
-     
-        count = 0 ;
+        vector<int>freq(256,0);
 
-        for(int k = 0; k<256;k++){
-            freq[k] = 0 ;
-        }
+        int maxcount = 0 ;
+        for(int i = 0 ; i<s.size();i++){
 
-        for(int j = i ; j<s.size();j++){
-            if(freq[s[j]]==0){
-                count++;
-                freq[s[j]]++;
+
+            for(int k = 0 ; k<256;k++){
+                freq[k] = 0 ;
             }
-            else{
-               
+            int count = 0 ;
+            for(int j = i ; j<s.size();j++){
+                if(freq[s[j]]==0){
+                    count++;
+                    freq[s[j]]++;
+                }
+                else{
+                    count = j -i;
                     break ;
                 }
+              
             }
-
-             if(maxcount<count){
-                    maxcount = count ;
-            }
+             if(count>maxcount){
+                maxcount = count ;
+             }
             
         }
-
-    
-return maxcount ;
+        return maxcount ;
     }
 };
