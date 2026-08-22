3d060daa5175d516c7790ad55c4c3cd0904f27fa
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-22 12:07:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false ;
+        }
         vector<int>freq(256,0);
 
         for(int i = 0 ; i<s.size();i++){
