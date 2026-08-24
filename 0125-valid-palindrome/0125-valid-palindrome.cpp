17d7056a5a5e8bcae482f772c9bfdb696380e4aa
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-24 18:16:06 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-      
- string ans = "";
-
-        for(int i = 0 ; i<s.size();i++){
-            if(s[i]>='A' && s[i]<='Z'){
-                s[i] = s[i]+32 ;
-                ans+=s[i];
-            }
-            else if(s[i]>='a'&& s[i]<='z'){
-                ans+=s[i];
-            }
-            else if(s[i]>='0' && s[i]<='9'){
-                ans+=s[i];
-            }
-        }
-
-        
-    int i = 0 ;
-    int j = ans.size()-1 ;
-    while(i<j){
-        if(ans[i]!=ans[j]){
-return false ;
-        }
-        i++;
-        j--;
+bool isvalid(char s ){
+    if( (s>='a' && s<='z') || (s>='A' && s<='Z') || (s>='0' && s<='9')){
+        return true ;
     }
-    return true ;
+return false ;
+}
+char makelower(char s ){
+    if(s>='A' && s<='Z'){
+        return s = s+32 ;
+    }
+    return s ;
+}
+    bool isPalindrome(string s) {
+        int i = 0 ; 
+        int j = s.size()-1 ;
+
+        while(i<j){
+
+            while(i<j && !isvalid(s[i])){
+                i++;
+            }
+
+            while(i<j && !isvalid(s[j])){
+                j--;
+            }
+
+            if(makelower(s[i])!=makelower(s[j])){
+                return false ;
+            }
+            i++;
+            j--;
+        }
+        return true ;
     }
 };
