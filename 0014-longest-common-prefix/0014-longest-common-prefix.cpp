53ebba2f7b56c9a1ab7175db1eb80e7ffaa4305a
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-24 17:42:19 +0530

    Time: 0 ms (100%), Space: 12.1 MB (25.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         sort(strs.begin(),strs.end());
+
         string first = strs[0];
-        string last = strs[strs.size()-1];
+        string second = strs[strs.size()-1];
         string ans = "";
-        for(int i = 0 ; i<min(first.size(),last.size());i++){
-            if(first[i]==last[i]){
-            ans+=first[i];
+        for(int i = 0 ; i<min(second.size(),first.size());i++){
+            if(first[i]==second[i]){
+                ans+=first[i];
             }
             else{
-                break;
+                break ;
             }
         }
         return ans ;
