5507fbf0e35b180da1af7ecf7ac82bdacc5cbcf0
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-08-31 19:20:41 +0530

    Time: 3 ms (14.38%), Space: 11.9 MB (71.7%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-
-        vector<int>freq ;
-        int count = 1 ;
-        for(int i = 1 ; i<arr.size();i++){
-            if(arr[i]==arr[i-1]){
-                count++;
-            }
-            else{
-                freq.push_back(count);
-                count = 1 ;
-            }
-
+        unordered_map<int,int>mp ;
+        unordered_set<int>s ;
+        for(int i = 0 ; i<arr.size();i++){
+            mp[arr[i]]++;
         }
-    freq.push_back(count);
-    sort(freq.begin(),freq.end());
-        for(int i = 1 ; i<freq.size();i++){
-            if(freq[i]==freq[i-1]){
-                return false ;
-            }
+
+        for(auto x : mp){
+            s.insert(x.second);
         }
-        return true ;
+        if(mp.size()==s.size()){
+            return true ;
+        }
+        else{
+            return false ;
+        }
     }
 };
