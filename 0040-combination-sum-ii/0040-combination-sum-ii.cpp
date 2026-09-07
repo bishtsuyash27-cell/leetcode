5014c73441adc0832787ed3b57d0e06f78ed7b19
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-07 16:28:45 +0530

    Time: 0 ms (100%), Space: 14 MB (84.38%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-void solve(vector<int>&candidates,int target,vector<int>&ans,vector<vector<int>>&finalans,int index){
+void solve(vector<int>&candidates,int target,vector<int>&ans,vector<vector<int>>&finalans,int index ){
     if(target==0){
         finalans.push_back(ans);
         return ;
     }
-    if(index==candidates.size() || target<0){
+    if(target<0 || index==candidates.size()){
         return ;
     }
-    
-  
     ans.push_back(candidates[index]);
     solve(candidates,target-candidates[index],ans,finalans,index+1);
     ans.pop_back();
 
-     int j = index+1 ;
-    while(j<candidates.size()&& candidates[j]==candidates[j-1]){
+    int j = index+1 ;
+    while(j<candidates.size() && candidates[j]==candidates[j-1]){
         j++;
     }
-   
     solve(candidates,target,ans,finalans,j);
 }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
@@ -27,5 +24,6 @@ void solve(vector<int>&candidates,int target,vector<int>&ans,vector<vector<int>>
         vector<vector<int>>finalans ;
         solve(candidates,target,ans,finalans,0);
         return finalans ;
+        
     }
 };
