6b85f7002891bc04aff39aff2506b654571b190f
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-08 15:03:47 +0530

    Time: 0 ms (100%), Space: 8.9 MB (40.35%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-void solve(int k , int n , vector<int>&ans , vector<vector<int>>&finalans,int index){
+void solve(int k , int n , vector<int>&ans,vector<vector<int>>&finalans,int index){
     if(k==0 && n==0){
         finalans.push_back(ans);
         return ;
     }
-    if(k==0 || n<0 || index>9){
+    if(k==0 || n==0){
         return ;
     }
-    ans.push_back(index);
-    solve(k-1,n-index,ans,finalans,index+1);
-    ans.pop_back();
 
-    solve(k,n,ans,finalans,index+1);
+    for(int i = index ; i<10;i++){
+        ans.push_back(i);
+        solve(k-1,n-i,ans,finalans,i+1);
+        ans.pop_back();
+    }
 }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int>ans ;
         vector<vector<int>>finalans ;
         solve(k,n,ans,finalans,1);
         return finalans ;
-
     }
 };
