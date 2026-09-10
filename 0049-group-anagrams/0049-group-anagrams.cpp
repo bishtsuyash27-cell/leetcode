ea996ade316f11afe18e47ce01e2ab3b1e86ff78
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-10 15:51:31 +0530

    Time: 192 ms (5%), Space: 24 MB (93.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans ;
+        vector<vector<string>>ans ;
+
         vector<string>key ;
-    for(int i = 0 ; i<strs.size();i++){
-        bool found = false ;
-        string s = strs[i];
-        sort(s.begin(),s.end());
-        for(int j = 0 ; j<key.size();j++){
-            if(s==key[j]){
-                ans[j].push_back(strs[i]);
-                found = true ;
-                break ;
+
+        for(int i = 0 ; i<strs.size();i++){
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            bool found = false ;
+            for(int j = 0 ; j<key.size();j++){
+                if(s==key[j]){
+                    ans[j].push_back(strs[i]);
+                    found = true ;
+                }
+            }
+            if(!found){
+                vector<string>newgroup;
+                newgroup.push_back(strs[i]);
+                ans.push_back(newgroup);
+                key.push_back(s);
             }
         }
-
-        if(!found){
-            vector<string>newgroup;
-            newgroup.push_back(strs[i]);
-            key.push_back(s);
-            ans.push_back(newgroup);
-        }
-    }
-return ans ;
-
+        return ans ;
     }
 };
