c0e229a2329e17bf7af4d0357ac7eb811fefd3e1
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-10 16:26:42 +0530

    Time: 27 ms (22.66%), Space: 27.1 MB (23.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -6,8 +6,15 @@ public:
         for(int i = 0 ; i<strs.size();i++){
             string s = strs[i];
             sort(s.begin(),s.end());
-
-            mp[s].push_back(strs[i]);
+            if(mp.find(s)==mp.end()){
+                vector<string>v ;
+                v.push_back(strs[i]);
+                mp[s] = v ;
+                
+            }else{
+                mp[s].push_back(strs[i]);
+            }
+            
         }
         for(auto x : mp){
             ans.push_back(x.second);
