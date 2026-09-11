607a16b10e725dc5f1c9509efe1134bac1330a0b
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-11 18:38:32 +0530

    Time: 28 ms (34.12%), Space: 23.9 MB (11.43%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -3,21 +3,26 @@ public:
     bool closeStrings(string word1, string word2) {
         if(word1.size()!=word2.size()){
             return false ;
-        }
-    unordered_map<char,int>mp1 ;
-    unordered_map<char,int>mp2 ;
 
-  vector<int>freq1;
-  vector<int>freq2;
-    for(int i = 0 ; i<word1.size();i++){
-        mp1[word1[i]]++;
-        mp2[word2[i]]++; 
-    }
-    for(auto x : mp1){
+        }
+        unordered_map<char,int>mp1 ;
+        unordered_map<char,int>mp2 ;
+
+        unordered_map<int,int>freq1 ;
+        unordered_map<int,int>freq2 ;
+
+        for(int i = 0 ; i<word1.size();i++){
+             mp1[word1[i]]++;
+             mp2[word2[i]]++;
+
+        }
+      
+      for(auto x  : mp1){
         if(mp2.find(x.first)==mp2.end()){
             return false ;
+
         }
-    }
+      }
 
     for(auto x : mp2){
         if(mp1.find(x.first)==mp1.end()){
@@ -25,18 +30,14 @@ public:
         }
     }
 
-for(auto x : mp1){
-    freq1.push_back(x.second);
+    for(auto x : mp1){
+        freq1[x.second]++;
     }
 
-    for(auto  x :  mp2){
-        freq2.push_back(x.second);
+    for(auto x : mp2){
+        freq2[x.second]++;
     }
 
-    sort(freq1.begin(),freq1.end());
-    sort(freq2.begin(),freq2.end());
-
-    return freq1==freq2 ;
+return freq1==freq2 ;
     }
-
 };
