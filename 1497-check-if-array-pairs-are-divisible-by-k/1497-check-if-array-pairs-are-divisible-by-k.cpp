58b075b35829c13542d2c7bb3dd7f04c407802cc
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-12 18:04:34 +0530

    Time: 4 ms (78.8%), Space: 76 MB (90.88%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         vector<int>remainder(k,0);
-        for(int i = 0 ; i<arr.size();i++){
+
+        for(int i = 0 ;i<arr.size();i++){
             int rem = arr[i]%k ;
             if(rem<0){
-                rem = rem+k ;
+                rem = rem + k ;
             }
             remainder[rem]++;
         }
-        if(remainder[0]%2!=0){
-                    return false ;
-                }
-        for(int i = 0 ; i<remainder.size();i++){
-            int required = (k - i)%k ;
-            
-            if(remainder[i]!=remainder[required]){
+    for(int i = 0 ; i<remainder.size();i++){
+        int required = (k-i)% k ;
+        if(i==required){
+            if(remainder[required]%2!=0){
                 return false ;
             }
         }
+        else{
+        if(remainder[i]!=remainder[required]){
+            return false ;
+        }
+        }
+    }
+
         return true ;
     }
 };
