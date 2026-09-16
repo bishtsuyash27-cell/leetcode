5f6f5a0b2e69947858d8223e46bcca08cfef4eec
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-16 16:05:38 +0530

    Time: 3 ms (64.5%), Space: 8.2 MB (11.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -7,7 +7,8 @@ int f(vector<int>&dp , int n ){
     if(dp[n]!=-1){
         return dp[n];
     }
-    return dp[n] = f(dp,n-1) + f(dp,n-2);
+    dp[n] = f(dp,n-1) + f(dp,n-2);
+    return dp[n];
 }
     int fib(int n) {
         vector<int>dp(n+1,-1);
