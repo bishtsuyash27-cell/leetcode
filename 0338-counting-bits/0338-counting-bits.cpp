f745dec9763353cf8ef4d846884a5ca175cef905
bishtsuyash27-cell <bishtsuyash27@gmail.com>
2026-09-19 20:42:02 +0530

    Time: 2 ms (35.15%), Space: 10.9 MB (32.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-int countbits(int x ){
+int countbits(int x,vector<int>&dp ){
     if(x==0){
         return 0 ;
     }
-    return countbits(x/2) + (x%2);
+    if(dp[x]!=-1){
+        return dp[x];
+    }
+    return dp[x] = countbits(x/2,dp) + (x%2);
 }
     vector<int> countBits(int n) {
         vector<int>ans(n+1,0);
-
+        vector<int>dp(n+1,-1);
         for(int i = 0 ; i<=n ;i++){
-            ans[i] = countbits(i);
+            ans[i] = countbits(i,dp);
         }
         return ans ;
     }
